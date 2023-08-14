98e673836cdfacf33aa5f2aa07eab4808db6a14b
lrljoe <joe@lowerrocklabs.com>
2023-08-14 04:07:24 +0100

    Tweaks

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const FormData = require('form-data');
 const core = require('@actions/core');
 const fs = require('fs');
 
-const host = core.getInput('host');
-const protocol = core.getInput('https') === 'true' ? 'https:' : 'http:';
-const path = core.getInput('path');
+const host = 'dev.larawatch.com';
+const protocol = 'https:';
+const path = '/api/uploadphpstan';
 const filePath = core.getInput('filePath');
 const data = core.getInput('data');
 
 console.info('endpoint', host + path);
 
 try {
-  fs.existsSync(filePath);
-  console.info('File found', filePath);
+    fs.existsSync(filePath);
+    console.info('File found', filePath);
 } catch (e) {
-  return core.setFailed('ERROR: file not found: ' + filePath);
+    return core.setFailed('ERROR: file not found: ' + filePath);
 }
 
 const form = new FormData();
@@ -22,18 +22,18 @@ form.append('file', fs.createReadStream(filePath));
 form.append('data', data);
 
 form.getLength(function (err, l) {
-  console.info('Sending file, size:', l + 'b');
+    console.info('Sending file, size:', l + 'b');
 });
 
-form.submit({host, protocol, path}, function (err, res) {
-  if (err) {
-    console.error(err);
-    return core.setFailed('Request failed');
-  }
+form.submit({ host, protocol, path }, function (err, res) {
+    if (err) {
+        console.error(err);
+        return core.setFailed('Request failed');
+    }
 
-  console.info('Response', res.statusCode, res.statusMessage);
+    console.info('Response', res.statusCode, res.statusMessage);
 
-  if (res.statusCode >= 400 && res.statusCode < 600) {
-    core.setFailed('Request failed');
-  }
+    if (res.statusCode >= 400 && res.statusCode < 600) {
+        core.setFailed('Request failed');
+    }
 });
